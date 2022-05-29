4926b0b449bc3075188854e1b631123bf74e44aa
Berint Moffett <berint.moffett@gmail.com>
2022-05-29 00:50:30 -0600

    Updated some things to remove unnecessary code from the compiled version.

--- pdf-split.ts
+++ pdf-split.ts
@@ -1,83 +1,7 @@
 import { PDFDocument } from "pdf-lib";
 import * as fs from "fs";
 import * as path from "path";
-
-const test: Array<typeof _splitObj> = [
-    {
-        startPage: 1,
-        title: "1st Eb Alto Saxophone (Clarinet)",
-    },
-    {
-        startPage: 3,
-        title: "2nd Eb Alto Saxophone (Clarinet)",
-    },
-    {
-        startPage: 5,
-        title: "1st Bb Tenor Saxophone",
-    },
-    {
-        startPage: 7,
-        title: "2nd Bb Tenor Saxophone",
-    },
-    {
-        startPage: 9,
-        title: "Eb Baritone Saxophone",
-    },
-    {
-        startPage: 11,
-        title: "1st Bb Trumpet",
-    },
-    {
-        startPage: 12,
-        title: "2nd Bb Trumpet",
-    },
-    {
-        startPage: 13,
-        title: "3rd Bb Trumpet",
-    },
-    {
-        startPage: 14,
-        title: "4th Bb Trumpet",
-    },
-    {
-        startPage: 16,
-        title: "1st Trombone",
-    },
-    {
-        startPage: 18,
-        title: "2nd Trombone",
-    },
-    {
-        startPage: 20,
-        title: "3rd Trombone",
-    },
-    {
-        startPage: 22,
-        title: "Piano",
-    },
-    {
-        startPage: 25,
-        title: "Guitar Chords",
-    },
-    {
-        startPage: 26,
-        title: "Guitar",
-    },
-    {
-        startPage: 28,
-        title: "Bass",
-    },
-    {
-        startPage: 30,
-        title: "Drums",
-    },
-];
-
-const _splitObj: { startPage: number; title: string } = {
-    // For typing purposes
-    startPage: 0,
-    title: "undefined",
-};
+import { _splitObj } from "./tests";
 
 async function loadPdf(filename: string): Promise<PDFDocument> {
     // loads a pdf by filename
@@ -176,5 +100,3 @@ async function splitPdf(
 }
 
 module.exports = splitPdf;
-
-splitPdf("./April in Paris.pdf", test, "./output");
---
+++ tests.ts
@@ -0,0 +1,78 @@
+const _splitObj: { startPage: number; title: string } = {
+    // For typing purposes
+    startPage: 0,
+    title: "undefined",
+};
+
+const test: Array<typeof _splitObj> = [
+    {
+        startPage: 1,
+        title: "1st Eb Alto Saxophone (Clarinet)",
+    },
+    {
+        startPage: 3,
+        title: "2nd Eb Alto Saxophone (Clarinet)",
+    },
+    {
+        startPage: 5,
+        title: "1st Bb Tenor Saxophone",
+    },
+    {
+        startPage: 7,
+        title: "2nd Bb Tenor Saxophone",
+    },
+    {
+        startPage: 9,
+        title: "Eb Baritone Saxophone",
+    },
+    {
+        startPage: 11,
+        title: "1st Bb Trumpet",
+    },
+    {
+        startPage: 12,
+        title: "2nd Bb Trumpet",
+    },
+    {
+        startPage: 13,
+        title: "3rd Bb Trumpet",
+    },
+    {
+        startPage: 14,
+        title: "4th Bb Trumpet",
+    },
+    {
+        startPage: 16,
+        title: "1st Trombone",
+    },
+    {
+        startPage: 18,
+        title: "2nd Trombone",
+    },
+    {
+        startPage: 20,
+        title: "3rd Trombone",
+    },
+    {
+        startPage: 22,
+        title: "Piano",
+    },
+    {
+        startPage: 25,
+        title: "Guitar Chords",
+    },
+    {
+        startPage: 26,
+        title: "Guitar",
+    },
+    {
+        startPage: 28,
+        title: "Bass",
+    },
+    {
+        startPage: 30,
+        title: "Drums",
+    },
+];
+
+export { _splitObj, test };
